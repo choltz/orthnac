cbd1c566f3e8c069f0ebf329f6b23c30568e4a8c
Chris Holtz <choltz@gmail.com>
2016-02-15 09:47:23 -0500

    More transaction test definitions

--- test/services/import_transactions_test.rb
+++ test/services/import_transactions_test.rb
@@ -3,6 +3,25 @@ require_relative '../test_helper'
 module Services
   class ImportTransactionsTest < ActiveSupport::TestCase
     context 'import transactions sevice tests' do
+      should 'import the transactions in the file' do
+
+      end
+
+      should 'not import overlapping transactions' do
+
+      end
+
+      should 'raise an error if there are formatting problems in the csv file' do
+
+      end
+
+      should 'raise an error if no file is provided' do
+
+      end
+
+      should 'raise an error if the file does not exist' do
+
+      end
     end
   end
 end
