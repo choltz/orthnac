c7cace2ed0cc1eb5e1268a1ecaf52932bb20b027
Chris Holtz <choltz@gmail.com>
2016-02-24 22:11:31 -0500

    Modularized transaction scopes a bit

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,15 @@
+# Public: Transaction model. Contains payments, expenditures, and credits
 class Transaction < ActiveRecord::Base
+  # Return all transactions between the specified start and end date
+  scope :between, ->(start_date, end_date) {
+    where('transaction_at between ? and ?', start_date, end_date)
+  }
+
+  # Return all transactions, except for payments, since he beginning of the
+  # month
   scope :monthly_to_date, -> {
-    where("transaction_type <> 'Payment'
-           and transaction_at between ? and ?", Date.today.beginning_of_month, Date.today)
+    where("transaction_type <> 'Payment'")
+      .between(Date.today.beginning_of_month, Date.today)
   }
 
   # Public: Return the amount of expenditures that have occurred since the
