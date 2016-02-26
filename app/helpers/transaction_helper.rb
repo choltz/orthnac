2c25def36307592633968c205da6c0f36e2c998e
Chris Holtz <choltz@gmail.com>
2016-02-25 23:11:54 -0500

    Formatted currency on the transactions page

--- app/helpers/transaction_helper.rb
+++ app/helpers/transaction_helper.rb
@@ -1,9 +1,8 @@
 module TransactionHelper
+  # Public: Format a decimal value into currency; style it in a div tag
   def format_amount(amount)
-    if amount < 0
-      "<span class=\"negative-amount\">(#{amount.to_s.delete('-')})</span>".html_safe
-    else
-      amount
-    end
+    formatted_amount = number_to_currency(amount, negative_format: "(%u%n)")
+    css_class        = amount < 0 ? 'negative-amount' : ''
+    "<span class=\"#{css_class}\">#{formatted_amount}</span>".html_safe
   end
 end
