50b942c4418cb6f4bb95fa7032bc16cfc15e972d
Chris Holtz <choltz@gmail.com>
2016-03-03 23:39:30 -0500

    Added comments to the Setting model

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,26 +1,32 @@
+# Public: Store app-level values
 class Setting < ActiveRecord::Base
   validates :statement_start_day, presence: true
 
   class << self
+    # use this class as a singleton
     def instance
       @instance ||= first
     end
 
+    # Public: Return the next statement billing date
+    #
+    # Returns: Date
     def next_statement_billing_date
       date = Date.today.beginning_of_month + Setting.statement_start_day - 1
       date = date + 1.month if date < Date.today
       date
     end
 
+    # Public: Get the stored statement start day.
+    #
+    # Returns: Integer
     def statement_start_day
       instance.statement_start_day
     end
 
+    # Public: Get the start and end dates for the current billing cycle
     def current_statement_dates
-      date = DateTime.new Date.today.year, Date.today.month, instance.statement_start_day
-      start_date = date > Date.today ? date - 1.month : date
-
-      [start_date, start_date + 1.month]
+      [next_statement_billing_date - 1.month, next_statement_billing_date]
     end
   end
 end
