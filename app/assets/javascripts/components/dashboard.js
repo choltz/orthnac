bf124f3b311e6f12488a93df8a31477df448679e
Chris Holtz <choltz@gmail.com>
2016-10-25 22:34:36 -0400

    Added comments to vue.js component

--- app/assets/javascripts/components/dashboard.js
+++ app/assets/javascripts/components/dashboard.js
@@ -1,18 +1,27 @@
+// Graph component: use google graph APi to display summary spending information
+//
+// id - identifing element used by google graph api to place the rendered
+//      content in the correct container
 Vue.component('graph', {
-  template: '<div :id="id">A2 custom component!</div>',
+  template: '<div :id="id"></div>',
   props: {
     id: { default: null }
   },
-  data: {
-    graphData: null
+  data: function() {
+    return   {
+      // Holds onto the data returned from the server
+      graphData: null
+    };
   },
   methods: {
+    // Initialize the google graph render workflow
     beginRender: function(data) {
       this.graphData = data;
 
       google.charts.load('current', {'packages':['corechart']});
       google.charts.setOnLoadCallback(this.drawChart);
     },
+    // Set graph render details
     drawChart: function() {
       this.graphData.unshift(['day', 'spent', 'total', 'max', 'projected']);
       this.graphData = google.visualization.arrayToDataTable(this.graphData);
@@ -28,6 +37,7 @@ Vue.component('graph', {
       var chart = new google.visualization.AreaChart(document.getElementById(this.id));
       chart.draw(this.graphData, options);
     },
+    // Retrieve graph data from the server
     getGraphData: function() {
       $.getJSON('api/dashboard/graph', this.beginRender );
     }
